76da002e0822ff9ca8d021036db7e2dda0f99b94
Samuel Lee <lee.sam1492@gmail.com>
2023-04-02 17:48:25 -0600

    installed react responive carousel. created carousel component. added router provider to app.tsx. imported carousel to home page. add css to app. added theme colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { RouterProvider } from "react-router-dom";
 import { NavBar } from "./components/navBar/NavBar";
+import { router } from "./router";
 import "./styles/App.scss";
 
 function App() {
   return (
     <div className="App">
+      <RouterProvider router={router} />
       <NavBar />
     </div>
   );
--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,7 +1,15 @@
+import { HomeCarousel } from "../../components/homeCarousel/HomeCarousel";
 import "./HomePage.scss";
 
 const HomePage: React.FC = () => {
-  return <div className="homePage"></div>;
+  return (
+    <div className="homePage">
+      <div style={{ marginBottom: "40px" }}>
+        <HomeCarousel />
+      </div>
+      <div className="test"></div>
+    </div>
+  );
 };
 
 export default HomePage;

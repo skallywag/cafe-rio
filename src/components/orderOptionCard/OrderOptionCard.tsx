d2c3dd5dc9d90df4a181feadd32afd5406ecdcf6
Samuel Lee <lee.sam1492@gmail.com>
2023-04-05 04:34:33 -0600

    Created orderOptionCard.tsx and scss. Added component interface types.

---
+++ src/components/orderOptionCard/OrderOptionCard.tsx
@@ -0,0 +1,23 @@
+import "./OrderOptionCard.scss";
+
+interface OptionCardProps {
+  id: number;
+  imageUrl: string;
+  saleItem: string;
+  isSelected: boolean;
+  onClick?: () => void;
+}
+
+const OptionCard: React.FC<OptionCardProps> = (props) => {
+  return (
+    <div className="optionCard" onClick={props.onClick}>
+      <img src={props.imageUrl} alt="tortilla" />
+      {props.isSelected && (
+        <img src="https://www.caferio.com/svg/item-select.svg" />
+      )}
+
+      <h4 style={{ textAlign: "center" }}>{props.saleItem}</h4>
+    </div>
+  );
+};
+export { OptionCard };
